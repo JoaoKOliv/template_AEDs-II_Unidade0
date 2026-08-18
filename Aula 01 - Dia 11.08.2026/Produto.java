9fb7815e97a7733147bf83e6b4d2a03279e7cc9b
Joao K Oliveira <165311816+JoaoKOliv@users.noreply.github.com>
2026-08-18 18:47:55 -0300

    Aula dia 11

---
+++ Aula 01 - Dia 11.08.2026/Produto.java
@@ -0,0 +1,40 @@
+import java.text.NumberFormat;
+
+public class Produto {
+	
+	private static final double MARGEM_PADRAO = 0.2;
+	private String descricao;
+	public double precoCusto;
+	public double margemLucro;
+	
+	private void init(String desc, double precoCusto, double margemLucro) {
+		
+		if ((desc.length() >= 3) && (precoCusto > 0.0) && (margemLucro > 0.0)) {
+			this.descricao = desc;
+			this.precoCusto = precoCusto;
+			this.margemLucro = margemLucro;
+		} else {
+			throw new IllegalArgumentException("Valores inválidos para os dados do produto.");
+		}
+	}
+	
+	public Produto(String desc, double precoCusto, double margemLucro) {
+		init(desc, precoCusto, margemLucro);
+	}
+	
+	public Produto(String desc, double precoCusto) {
+		init(desc, precoCusto, MARGEM_PADRAO);
+	}
+	
+	public double valorDeVenda() {
+		return (precoCusto * (1.0 + margemLucro));
+	}
+	
+    @Override
+	public String toString() {
+    
+    NumberFormat moeda = NumberFormat.getCurrencyInstance();
+    
+		return String.format("NOME: " + descricao + ": " + moeda.format(valorDeVenda()));
+	}
+}
